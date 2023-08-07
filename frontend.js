f5aba2e8c7b829837d5a2c539cab195e94b2c664
Razi momin <razi.1088@gmail.com>
2023-08-08 01:18:31 +0530

    timer add

--- frontend.js
+++ frontend.js
@@ -29,7 +29,7 @@ socket.on('matchFound', (data) => {
 })
 socket.on('updateBoard', (data) => {
     const { currentPLayerSocketId, boardData, matchEndTime } = data;
-    timeDifference(matchEndTime, counter);
+    timeDifference(matchEndTime, 'counter');
     let turnText = 'Opponent turn'
     if (currentPLayerSocketId === socket.id) {
         turnText = 'Your Turn'
@@ -60,7 +60,37 @@ socket.on('searchUser', (text) => {
 // })
 
 function timeDifference(date, match_id) {
+    var countDownDate = new Date(date).getTime();
 
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        days = (days > 0) ? days + "d " : ''
+        hours = (hours > 0) ? hours + "h " : ''
+        minutes = (minutes > 0) ? minutes + "m " : ''
+        seconds = (seconds > 0) ? seconds + "s " : ''
+        document.getElementById(match_id).innerHTML = days + hours
+            + minutes + seconds;
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById(match_id).innerHTML = "EXPIRED";
+        }
+    }, 1000);
 }
 function pad2(number) {
 
